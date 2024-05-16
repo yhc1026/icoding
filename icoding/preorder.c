c506ee07fe80f64dd72f72a7bc09adbe74bf987e
yhc1026 <478946668@qq.com>
2024-05-16 13:55:24 +0800

    2024/5/16

--- icoding/preorder.c
+++ icoding/preorder.c
@@ -31,3 +31,11 @@ bool push(Stack *S, ElemType x);  // x 入栈
 bool pop(Stack *S, ElemType *px); // 出栈，元素保存到px所指的单元，函数返回true,栈为空时返回 false
 bool top(Stack *S, ElemType *px); // 获取栈顶元素，将其保存到px所指的单元，函数返回true，栈满时返回 false
 bool is_empty(Stack *S);          // 栈为空时返回 true，否则返回 false
+
+#include "bitree.h" //请不要删除，否则检查不通过
+#include <stdio.h>
+#include <stdlib.h>
+
+void pre_order(BiTree root)
+{
+}
